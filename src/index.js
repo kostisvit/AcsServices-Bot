9fe511dbc36e213f1167c427d2c0e8b21480dd02
kostasvit <kostasvit@gmail.com>
2022-03-14 21:31:51 +0200

    bot changes

--- src/index.js
+++ src/index.js
@@ -1,46 +1,18 @@
-const { router, text} = require('bottender/router');
+const { ConsoleBot } = require("bottender");
+
 const unknownResponse = "Sorry.\n I don't understand.";
 
-async function SayHi(context) {
-  await context.sendText('Greetings!!! \n How can i help you?');
-  await context.sendText('What is the name you are looking for?');
-  
+async function SayHello(context) {
+  await context.sendText('Greetings!!\n How can I help you?');
+  await context.sendText('What is the name you are looking for?')
+
   context.setState({
     current_state: 'waiting_name'
-  })
-}
+})
 
-const employees = {
-  'kostas': {
-    'tel': '27420 21345'
-  },
-
-}
-
-
-async function Unknown(context) {
-  if ( context.event.isText ) {
-    const input = context.event.text.toLowerCase();
-    if ( context.state.current_state === 'waiting_name' && employees[input] ) {
-      await context.sendText(employees[input].tel);
-      await context.sendText('Type user\'s email');
-
-      context.setState({
-        current_state: 'waiting_email'
-      })
-    } else if ( input === 'how is the weather today?') {
-
-    } else if ( input === 'what is my name?') {
-      await context.getUserProfile().then(async function(user){
-        await context.sendText(user.name);
-      })
-    } else {
-      await context.sendText(unknownResponse);
-    }
-    
-  } else {
-    await context.sendText(unknownResponse);
-  }
+  context.setState({
+    current_state: 'waiting_email'
+})
 }
 
 const hiArray = [
@@ -49,28 +21,50 @@ const hiArray = [
   'hey'
 ]
 
-module.exports = async function App(context) {
-  // if (context.event.text == 'hi') {
-  //   return SayHi;
-  // }
-  // return Unknown;
+// const employeeInfo = [
+//   'phone',
+//   'mobile'
+// ]
 
-  if ( context.event.isText && hiArray.includes(context.event.text.toLowerCase())) {
-    return SayHi;
+const employees = {
+  'kostas': {
+    'tel': '27420-25537',
+    'mobile': '6936486424',
+    'email': 'kostasvit@acsservices.gr'
+  }
+}
+
+
+
+async function Unknown(context) {
+  if ( context.event.isText ) {
+    const input = context.event.text.toLowerCase();
+    if ( context.state.current_state = 'waiting_name' && employees[input] ){
+      await context.sendText(employees[input].tel);
+      await context.sendText('Something else?');
+
+      context.setState({
+        current_state: 'waiting_name'
+      })
+    } else if ( input === 'email') {
+      
+      
+      
+
+      // console.log(employees.kostas.email);
+        
+
+    }
+    else {
+      await context.sendText(unknownResponse);
+    }
+  }
+}
+
+module.exports = async function App(context) {
+  if ( context.event.isText && hiArray.includes(context.event.text.toLowerCase())){
+    return SayHello
   }
 
-  // if ( context.event.isText && 
-  //   (context.event.text.toLowerCase() === 'hi' || 
-  //   context.event.text.toLowerCase() === 'hello' ||
-  //   context.event.text.toLowerCase() === 'hey') ) {
-  //   return SayHi;
-  // }
-
-  // return router([
-  //   text(['hi','hello','hey'], SayHi),
-  //   text('*', Unknown),
-  // ]);
-
-  return Unknown;
+  return Unknown
 };
-
