edaf35f857f7763cc6dbd17d60777642f67df94f
kostasvit <kostasvit@gmail.com>
2022-03-23 19:54:27 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ const { ConsoleBot } = require("bottender");
 const unknownResponse = "Sorry.\n I don't understand.";
 
 async function SayHello(context) {
-  await context.sendText('Greetings!!\n How can I help you?');
-  await context.sendText('What is the name you are looking for?')
+  //await context.sendText('Greetings!!\n How can I help you?');
+  await context.getUserProfile().then(async function(user){
+  await context.sendText("Greetings!!" + ' ' + user.name);
+  })
+  await context.sendText('What is the employee you are looking for?')
 
   context.setState({
     current_state: 'waiting_name'
@@ -25,8 +28,9 @@ const hiArray = [
 const employees = {
   'kostas': {
     'tel': '27420-25537',
-    'mobile': '6936486424',
-    'email': 'kostasvit@acsservices.gr'
+    'mobile': '6936-486424',
+    'email': 'kostasvit@acsservices.gr',
+    'site': 'https://www.acsservices.gr'
   },
   'john': {
     'tel': '22220-55555',
@@ -41,7 +45,7 @@ async function Unknown(context) {
   if ( context.event.isText ) {
     const input = context.event.text.toLowerCase();
     if ( context.state.current_state == 'waiting_name' && employees[input] ){
-      await context.sendText(employees[input].tel);
+      await context.sendText("Tel:" + ' ' + employees[input].tel);
       
       await context.sendText('Something else?');
 
@@ -53,7 +57,7 @@ async function Unknown(context) {
     } else if ( context.state.current_state == 'waiting_email' && input === 'email') {
       const employee = context.state.employee;
       const email = employees[employee].email;
-      await context.sendText(email);
+      await context.sendText("E-mail:" + ' ' + email);
 
       // console.log(employees.kostas.email);
       context.setState({
@@ -62,12 +66,20 @@ async function Unknown(context) {
     } else if ( context.state.current_state == 'waiting_mobile' && input ==='mobile'){
       const employee = context.state.employee;
       const mobile = employees[employee].mobile;
-      await context.sendText(mobile);
-    }
+      await context.sendText("Mobile:" + ' ' + mobile);
 
+      context.setState({
+        current_state: 'waiting_url',
+      })
+    } else if( context.state.current_state == 'waiting_url' && input === 'site'){
+      const employee = context.state.employee;
+      const site = employees[employee].site;
+      await context.sendText("Site:" + ' ' + site);
+    }
     else {
       await context.sendText(unknownResponse);
     }
+   
   }
 }
 
